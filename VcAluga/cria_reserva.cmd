395114348078f2d2aac3039bd3d5940234ec436b
daniel.fsalves@gmail.com <daniel.fsalves@gmail.com@50857316-cf60-33c1-494d-e3e64b7ce3d8>
2010-11-29 01:10:16 +0000

    Trabalho 5

--- VcAluga/cria_reserva.cmd
+++ VcAluga/cria_reserva.cmd
@@ -1,15 +1,33 @@
+--Hora agora
 !create agora:dataHora
 !set agora.value := (23 * 60) + (11 * 30 * 24 * 60) + (2010 * 365 * 24 * 60)
 !set agora.agora := agora
 
+--data da emissao do CC
 !create emissaoCC:dataHora
 !set emissaoCC.value := (16 * 60) + (7 * 30 * 24 * 60) + (2007 * 365 * 24 * 60)
 !set emissaoCC.agora := agora
 
+--validade do CC
 !create validCC:dataHora
 !set validCC.value := (14 * 60) + (7 * 30 * 24 * 60) + (2011 * 365 * 24 * 60)
 !set validCC.agora := agora
 
+--data da emissao da carteira
+!create emissaoCarteira:dataHora
+!set emissaoCarteira.value := (16 * 60) + (7 * 30 * 24 * 60) + (2007 * 365 * 24 * 60)
+!set emissaoCarteira.agora := agora
+
+--validade da carteira
+!create validCarteira:dataHora
+!set validCarteira.value := (14 * 60) + (7 * 30 * 24 * 60) + (2011 * 365 * 24 * 60)
+!set validCarteira.agora := agora
+
+!create carteira:carteira_habilitacao
+!set carteira.emissao := emissaoCarteira
+!set carteira.validade := validCarteira
+
+--o CC do Ze Sa
 !create cartao:CC
 !set cartao.possuidor := 'ze_sa'
 !set cartao.disponibilidade := 10000.00
@@ -17,3 +35,40 @@
 !set cartao.validade := validCC
 !set cartao.emissao := emissaoCC
 
+-- O Famoso Ze Sa!
+!create ze_sa : cliente
+!set ze_sa.nome := 'Ze Sa'
+!set ze_sa.cpf := '11111111111'
+!set ze_sa.endereco := 'Rua dos Bobos, n. 0'
+!set ze_sa.idade := 42
+!set ze_sa.cartao := cartao
+!set ze_sa.condicao := 'normal'
+!set ze_sa.apto := true
+
+!insert (ze_sa, carteira) into tem
+
+--Reserva de Ze Sa
+!create dReservaZe : dataHora
+!set	dReservaZe.value := (20 * 60) + (3 * 30 * 24 * 60) + (2010 * 365 * 24 * 60)
+!set	dReservaZe.agora := agora
+
+!create dDevZe : dataHora
+!set	dDevZe.value := (23 * 60) + (3 * 30 * 24 * 60) + (2010 * 365 * 24 * 60)
+!set	dDevZe.agora := agora
+
+--Agencia
+!create jujuba : agencia
+!set	jujuba.localizacao := 'Jujubalandia'
+
+!create reservaZe : reserva between (ze_sa, jujuba)
+--!insert (ze_sa, jujuba) into reservaZe : reserva
+!set	reservaZe.dataHora_reserva := dReservaZe
+!set	reservaZe.dataHora_devolucao := dDevZe
+
+--Grupo de carro Categoria A
+
+!create a : grupo_carro
+!set	a.categoria := 'Conversivel'
+
+-- garantia do cartao para a reserva
+!insert (reservaZe, cartao) into garante
